8ddf43a3079790b57c2d8229eaad0a4c87615147
qcgm1978 <zhanghl@strongunion.com.cn>
2018-05-16 20:53:56 +0800

    test

--- _test_/matter-js.spec.ts
+++ _test_/matter-js.spec.ts
@@ -1,4 +1,5 @@
 import * as Matter from 'matter-js'
 it(`The Matter.Render module is a simple HTML5 canvas based renderer for visualising instances of Matter.Engine`, () => {
     expect(Matter.Render).toBeInstanceOf(Object)
+    expect(Matter.Render.create).toBeInstanceOf(Function)
 });

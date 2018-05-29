1a5c5d401f2a988ac2b2d84e0bf5e84cf55dc944
qcgm1978 <zhanghl@strongunion.com.cn>
2018-05-29 20:02:16 +0800

    a

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: { home: "./html/region" },
+    output: {
+        filename: "./html/region.js",
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.tsx?$/, use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ], include: path.resolve(__dirname, 'html'),
+            }
+        ]
+    }
+};
